c777d6b0865befa1545e18556923a578c56387f9
unknown <efrancis@.rspl.local>
2011-10-14 17:43:57 +0530

    app name changed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Hh::Application.config.session_store :cookie_store, :key => '_hh_session'
+SampleApp::Application.config.session_store :cookie_store, :key => '_hh_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
